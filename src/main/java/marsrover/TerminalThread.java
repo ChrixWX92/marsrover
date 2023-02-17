6689adffbb46a0b3ff359780fb7a202eb00da2ad
ChrixWX92 <chris@wards-net.co.uk>
2023-02-17 21:24:04 +0000

    fix: Incorrect rotation on Terminal command string execution

--- src/main/java/marsrover/TerminalThread.java
+++ src/main/java/marsrover/TerminalThread.java
@@ -5,6 +5,8 @@ import marsrover.model.Movement;
 
 import java.io.*;
 
+import static marsrover.model.Movement.MovementType.*;
+
 public class TerminalThread implements Runnable {
 
     public static volatile boolean movementFlag = false;
@@ -22,18 +24,19 @@ public class TerminalThread implements Runnable {
             String instructions = reader.readLine().toUpperCase();
             for (char instruction : instructions.toCharArray()) {
                 Movement.MovementType direction = switch (instruction) {
-                    case 'M' -> Movement.MovementType.FORWARD;
-                    case 'B' -> Movement.MovementType.BACKWARD;
-                    case 'L' -> Movement.MovementType.TURN_LEFT;
-                    case 'R' -> Movement.MovementType.TURN_RIGHT;
+                    case 'M' -> FORWARD;
+                    case 'B' -> BACKWARD;
+                    case 'L' -> TURN_LEFT;
+                    case 'R' -> TURN_RIGHT;
                     default -> throw new IllegalStateException("Unexpected value: " + instruction);
                 };
                 while (movementFlag) {Thread.onSpinWait();}
-                world.getActiveMovable().move(direction, world.getMovementQuantum());
+
+                double distance = (direction == TURN_LEFT || direction == TURN_RIGHT) ? 90 : world.getMovementQuantum();
+                world.getActiveMovable().move(direction, distance);
                 Thread.sleep(1000);
             }
             int[] endCoords = ((Entity) world.getActiveMovable()).getCoordinates();
-//            System.out.println(((Entity) world.getActiveMovable()).model.getXform().getRotate());
             System.out.println(endCoords[0] + " " + endCoords[1]);
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
